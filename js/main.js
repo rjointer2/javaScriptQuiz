941b24f2f9ce308dd5aa7fd402819b3cf65f9503
Roody <rjointer2@gmail.com>
2021-02-23 01:38:44 -0800

    Ok, got the timer, the question structure working. Now to add to the if and else statement to make the question change after clicking one

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+/* HTML ELEMENT SELECTED */
+
+let score = document.querySelector('.score');
+let timer = document.querySelector('.timer');
+let questionBox = document.querySelector('.question_box');
+let answerBox = document.querySelector('.answer_box');
+
 /* START QUIZ BUTTON */
 
 let startBtn = document.querySelector('.startBtn')
@@ -6,16 +13,6 @@ startBtn.addEventListener('click', () => {
 })
 
 let startQuiz = () => {
-
-    /* HTML ELEMENT SELECTED */
-
-    let score = document.querySelector('.score');
-    let timer = document.querySelector('.timer');
-    let questionBox = document.querySelector('.question_box');
-    let answerBox = document.querySelector('.answer_box');
-
-
-
 
     /* TIMER */
 
@@ -31,76 +28,66 @@ let startQuiz = () => {
         }, 1000)
     } 
 
+    timerFunction(90)
+
     /* QUESTIONS AND ANSWERS */
 
- 
-    let radioBtn = document.createElement("input");
-    radioBtn.setAttribute("type", "radio");
-
-    questionBox.innerHTML = "What is Javascript?"
-    answerBox.innerHTML = "";
-
-    answerBox.appendChild(radioBtn);
+    quizInit(questions)
+    
 }
 
 
 /* QUESTION DATA STUCTURE */
 
-let questionArray = [
+const questions = [
     {
-        "question" : "What is Javascript?"
+      question: "What is Javascript?",
+      answers: {
+        a: "A Script From the Play Java",
+        b: "A Programming Language",
+        c: "A Coffee Brand"
+      }
     },
     {
-        "question" : "What is a Lexical Environment?"
+      question: "What does DOM mean?",
+      answers: {
+        a: "Document Object Model",
+        b: "Domino's Pizza",
+        c: "Don't Overuse Models"
+      }
     },
     {
-        "question" : "What is a arrow function>"
-    },
-    {
-        "question" : "What is the difference between let and var"
+      question: "What is the KISS principle?",
+      answers: {
+        a: "Kiss someone",
+        b: "Keep It Super Serious",
+        c: "Keep It Stupid Simple"
+      }
     }
-]
+];
 
-let answerArray = [
-    {
-        "answer" : [
-            "A", "B", "C"
-        ]
-    },
-    {
-        "answer" : [
-            "A", "B", "C"
-        ]
-    },
-    {
-        "answer" : [
-            "A", "B", "C"
-        ]
-    },
-    {
-        "answer" : [
-            "A", "B", "C"
-        ]
-    }
-]
+let quizInit = (data) => {
 
+    let ul = document.createElement("ul");
+    ul.setAttribute("class", "list");
+    answerBox.appendChild(ul)
 
-let buildQuestion = (qus, ans) => {
-    
-    qus.forEach((i) => {
-        console.log(i.question)
-    })
+    let choice1 = document.createElement("li");
+    let answer1 = document.createTextNode(data[0].answers.a);
+    choice1.appendChild(answer1);
+    ul.appendChild(choice1);
 
-    ans.forEach((i) => {
-        i.answer.forEach((j) => {
-            console.log(j)
-        })
-    })
+    let choice2 = document.createElement("li");
+    let answer2 = document.createTextNode(data[0].answers.b);
+    choice2.appendChild(answer2);
+    ul.appendChild(choice2);
 
-   
+    let choice3 = document.createElement("li");
+    let answer3 = document.createTextNode(data[0].answers.c);
+    choice3.appendChild(answer3);
+    ul.appendChild(choice3)
+
 }
-
-console.log(buildQuestion(questionArray, answerArray))
 
 
 
