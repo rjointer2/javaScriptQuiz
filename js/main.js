d079f153b2bf6301d957719d8fe5a48e74cf5add
Roody <rjointer2@gmail.com>
2021-02-25 16:31:31 -0800

    finished the code base, it's pretty much modular and object oriented as possible.

--- js/main.js
+++ js/main.js
@@ -61,9 +61,12 @@ class QuestionObject {
             btn.addEventListener('click', (e) => {
                 pagination()
                 if(btn.value == "true" ) {
-                    console.log('correct')
+                    console.log('correct');
+                    answerBox.innerHTML = "<h2>CORRECT</h2>"
                 } else {
-                    console.log('incorrect')
+                    time += -10
+                    console.log('incorrect');
+                    answerBox.innerHTML = "<h2>WRONG</h2>"
                 }
             })
 
@@ -138,7 +141,7 @@ let question4 = new QuestionObject("What is a lexical enviroment? ",
 let question5 = new QuestionObject('What does 2 + "2" equal in Javascript?',
     {
         "answer": "22 ",
-        "correct": false
+        "correct": true
     },
     {
         "answer": "NaN ",
@@ -146,7 +149,7 @@ let question5 = new QuestionObject('What does 2 + "2" equal in Javascript?',
     },
     {
         "answer": "4 ",
-        "correct": true
+        "correct": false
     }
 )
 
@@ -179,6 +182,18 @@ init.addEventListener('click', () => {
     question1.construct()
 })
 
+/* RESET QUIZ AND SAVE DATA */
+
+let reset = () => {
+    let formBtnText = document.createTextNode("Back to Quiz?");
+    let formBtn = document.createElement("button");
+    formBtn.appendChild(formBtnText);
+    answerBox.appendChild(formBtn);
+    formBtn.addEventListener('click', () => {
+        location.reload()
+    })
+}
+
 /* PAGINATION */
 
 let arr = [question1,
@@ -186,19 +201,84 @@ let arr = [question1,
     question3,
     question4,
     question5,
-    question6
+    question6,
+    null
 ]
+
 
 let pagination = () => {
 
-    if( counter > counter - 1 ) {
-        counter++
-        clearPage()
-        arr[counter].construct()
-        if( counter === 5 ) {
+    setTimeout(() => {
+
+
+        if( counter < 5 ) {
+            counter++
+            console.log(counter)
+            clearPage()
+            arr[counter].construct()
+        } else {
             console.log("test")
-            questionBox.innerHTML = "Congratz you finished the quiz! Here's your score! " + time
-            answerBox.innerHTML = "great"
+            clearPage()
+            let points = time
+            let save = () => {
+                let form = document.createElement('input');
+                let formBtnText = document.createTextNode("Submit");
+                let formBtn = document.createElement("button");
+                form.setAttribute("type", "text");
+                answerBox.appendChild(form);
+                formBtn.appendChild(formBtnText);
+                answerBox.appendChild(formBtn);
+                formBtn.addEventListener('click', () => {
+                    localStorage.setItem("name", form.value)
+                    localStorage.setItem("score", points)
+                    location.reload()
+                })
+            }
+    
+            if( time < 10 ) {
+                questionBox.innerHTML = "Congratz you finished the quiz! Here's your score! " + time + " <br><br>Javscript is tough and you will meet some tough paths. It's important to keep trying and learning!<br><br> Add your name to save your score! " 
+            }
+    
+            if( time < 20 ) {
+                questionBox.innerHTML = "Congratz you finished the quiz! Here's your score! " + time + " <br><br>Javascript is tough, but you just have to practice!<br><br> Add your name to save your score! " 
+            }
+    
+            if( time > 30 ) {
+                questionBox.innerHTML = "Congratz you finished the quiz! Here's your score! " + time + " <br><br>Nice try, but you can improve!<br><br> Add your name to save your score! " 
+            }
+    
+            if( time > 40) {
+                questionBox.innerHTML = "Congratz you finished the quiz! Here's your score! " + time + " <br><br>Good job, you did good!<br><br> Add your name to save your score! " 
+            }
+    
+            if( time > 65) {
+                questionBox.innerHTML = "Congratz you finished the quiz! Here's your score! " + time + " <br><br>Impressive, your pretty clever (ツ)!<br><br> Add your name to save your score! " 
+            }
+    
+            if( time > 80) {
+                questionBox.innerHTML = "SCORE => " + points + " :: IF ON THIS PAGE THIS IS FOR TESTING BY THE WEBMASTER OR YOUR A CHEATER ¯\_(ツ)_/¯"
+            }
+            answerBox.innerHTML = "Reset the Quiz to score higher or view the curent high score "
+            save()
+
+            timer.innerHTML = "Time's up!"
         }
-    } 
+
+    },800)
 }
+
+
+/* VIEW HIGHSCORES */
+
+let highScoresBtn = document.createElement('button');
+let highScoreBtnText = document.createTextNode('View High Scores');
+answerBox.appendChild(highScoresBtn).appendChild(highScoreBtnText);
+
+highScoresBtn.addEventListener('click', ()=> {
+    questionBox.innerHTML = "";
+    answerBox.innerHTML = "";
+
+    questionBox.innerHTML = "Most Recent Score! Can you do better?";
+    answerBox.innerHTML = localStorage.getItem("name") + " " +localStorage.getItem("score") + "<br><br>";
+    reset()
+})
